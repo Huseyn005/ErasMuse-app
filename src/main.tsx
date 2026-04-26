8f2ceac89d717f546d013e11f5a9fb31732f5f35
Huseyn005 <your-username@users.noreply.github.com>
2026-04-26 15:31:06 +0300

    feat: add header component with logo and title styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n";
+<div className="erasmuse-header">
+  <img src="/images/erasmuse-icon.jpg" alt="ERASMuse logo" />
+  <span>ERASMuse</span>
+</div>
 
 createRoot(document.getElementById("root")!).render(<App />);
